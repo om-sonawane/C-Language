528349ea1c888b045efaa1f7f60c6d73a21c28d9
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2022-01-18 11:26:01 -0800

    celsius to farhnhite

---
+++ celsius_to_fahrenheit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float celsius =3,far;
+	far= (celsius *9/5)+32;
+	printf(" the value of this celsius temprature in fahrenhite is %f ",far);
+	 
+	
+	
+	return 0;
+
+}
