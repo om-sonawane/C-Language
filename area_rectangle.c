1a2523f856d6a80da1c4d835933b6c505e7b8dee
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2022-01-18 11:25:16 -0800

    area

---
+++ area_rectangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+//To find the area of reactangle,
+int main()
+{    
+	int length,breadth;
+	
+	printf("what is the lenght of rectangle \n");
+	scanf("%d", &length);
+	
+	printf("what is the breadth of rectangle \n");
+	scanf("%d", &breadth);
+	
+	printf("area of your rectangle is %d", length*breadth);
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
